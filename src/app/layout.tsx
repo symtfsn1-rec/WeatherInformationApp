eed4558f52fd2e532847ce5dfcbece4512ddcdfb
“snmrec” <symtfsn1@gmail.com>
2026-08-23 16:03:43 +0900

    CursorFollower.tsxをcomponents/commonへ移動

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import CursorFollower from "@/components/CursorFollower";
+import CursorFollower from "@/components/common/CursorFollower";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
