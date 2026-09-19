e2623678021ac71f5fcb72190c7e6512a8f5ccde
“snmrec” <symtfsn1@gmail.com>
2026-09-19 14:37:18 +0900

    追従円修正

--- src/components/common/CursorFollower.tsx
+++ src/components/common/CursorFollower.tsx
@@ -20,13 +20,13 @@ export default function CursorFollower({size = 30, color = 'bg-linear-to-b from-
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
-  });
+  }, []);
 
   const halfSize = size / 2;
 
   return (
     <div
-      className={`fixed top-0 left-0 rounded-full pointer-events-none z-50 opacity-60 transition-transform ease-out ${delayDuration} ${color}`}
+      className={`hidden lg:block fixed top-0 left-0 rounded-full pointer-events-none z-50 opacity-60 transition-transform ease-out ${delayDuration} ${color}`}
       style={{
         width: `${size}px`,
         height: `${size}px`,
